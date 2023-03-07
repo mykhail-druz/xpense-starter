66ed2d1c6370671cd01737493b449471f17fd2df
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2023-03-07 17:23:47 +0200

    Add features and product

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+
+import { overview } from "../data";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  const { productImg } = overview;
+  return <section className="lg:min-h-[712px] bg-overview bg-cover bg-left-top pt-[30px] lg:pt-[87px]">
+    <div className="container mx-auto flex justify-end overflow-hidden">
+      <img src={productImg} alt="product image" data-aos="fade-up" data-aos-offset="300"/>
+    </div>
+  </section>;
 };
 
 export default Overview;
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from "react";
+import { pricing } from "../data";
+import { HiCheck, HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  const [index, setIndex] = useState(1);
+  const { title } = pricing;
+  return <section></section>;
 };
 
 export default Pricing;
